38405da927981339d11f42d2b1a93da68cdf8742
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-02-27 15:28:37 +0100

    add changes

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 
 const listaSpesa = ['verdura', 'carne', 'pasta', 'pane', 'carciofi'];
 
+const stampaLista = document.getElementById('lista_spesa');
+
 let i = 0;
 
-while( i < listaSpesa.length){
-  console.log(listaSpesa[i]);
+while(!(i == listaSpesa.length )){
+  
+  stampaLista.innerHTML += '<li>' + listaSpesa[i] + '</li>';
 
   i++;
 }
